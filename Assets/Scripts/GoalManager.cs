97d64aa01f60d9b018907e3f9753e040fcdaae47
badaueba <alan.develop@outlook.com>
2016-06-14 19:12:24 -0300

    goal manager

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalManager : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.gameObject.tag == "GoalArea") {
+			Debug.Log ("GoalArea: Gol");
+		}
+	}
+}
